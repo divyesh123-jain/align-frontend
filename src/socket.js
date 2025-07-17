d5ba651f11ccb4bc1a88c5519cc9ce92adec94a4
divyesh123-jain <divyeshjain318@gmail.com>
2025-07-17 18:48:25 +0530

    frontend

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
 
-const SOCKET_URL = 'http://localhost:4000';
+const SOCKET_URL = 'https://asign-backend.railway.internal';
 
 export const socket = io(SOCKET_URL);
