9afc5620102a96e5b05b524ac751c1f934c5dfd1
divyesh123-jain <divyeshjain318@gmail.com>
2025-07-17 19:22:41 +0530

    frontend

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
 
-const SOCKET_URL = 'https://asign-backend-production.up.railway.app'
+const SOCKET_URL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:4000';
 
 export const socket = io(SOCKET_URL);
